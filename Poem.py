e4fb948f9910cc973ff1d7d44e8013fad7868bd6
jass <0855>
2020-01-08 18:42:35 -0800

    Poem commit

---
+++ Poem.py
@@ -0,0 +1,6 @@
+print ("Roses are red "
+       "violets are blue "
+       "Sugar is sweet "
+       "But i have commitment issues"
+       "So I'd rather just be friends "
+       "At this point in our relationship.")
